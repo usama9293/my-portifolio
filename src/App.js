afe8946f3d6fabf34a631ed350690217761e338d
usama9293 <usamaarshad9293@gmail.com>
2024-08-08 13:30:55 +0500

    "design and implement navbar with materialUI"

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { createTheme, ThemeProvider, CssBaseline } from "@mui/material";
+import Header from "./components/Header";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: '"Playfair Display", serif',
+  },
+});
+
 function App() {
-  return <div className="App">app</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <Header /> {/* Place Header here if it should be on all pages */}
+        <Routes>
+          
+        </Routes>
+      </Router>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import { Link } from "react-router-dom";
+import Box from "@mui/material/Box";
+import { BoltRounded, FormatBold } from "@mui/icons-material";
+
+function Header() {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const toggleDrawer = (open) => () => {
+    setDrawerOpen(open);
+  };
+
+  const drawer = (
+    <Box
+      sx={{ width: 250 }}
+      role="presentation"
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      <List>
+        <ListItem button component={Link} to="/">
+          <ListItemText primary="Home" />
+        </ListItem>
+        <ListItem button component={Link} to="/about">
+          <ListItemText primary="About" />
+        </ListItem>
+        <ListItem button component={Link} to="/projects">
+          <ListItemText primary="Projects" />
+        </ListItem>
+        <ListItem button component={Link} to="/contact">
+          <ListItemText primary="Contact" />
+        </ListItem>
+      </List>
+    </Box>
+  );
+
+  return (
+    <>
+      <AppBar
+        position="static"
+        sx={{ backgroundColor: "white", color: "black", padding: "10px 0" }}
+      >
+        <Toolbar>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{
+              flexGrow: 1,
+              fontWeight: "bold",
+              fontSize: "1.5rem", // Increase font size
+              fontFamily: '"Playfair Display", serif', // Example font family
+            }}
+          >
+            My Portfolio
+          </Typography>
+          <Box sx={{ display: { xs: "none", md: "block" } }}>
+            <Button color="inherit" component={Link} to="/">
+              Home
+            </Button>
+            <Button color="inherit" component={Link} to="/about">
+              About
+            </Button>
+            <Button color="inherit" component={Link} to="/projects">
+              Projects
+            </Button>
+            <Button color="inherit" component={Link} to="/contact">
+              Contact
+            </Button>
+          </Box>
+          <IconButton
+            color="inherit"
+            edge="start"
+            sx={{ display: { xs: "block", md: "none" } }}
+            onClick={toggleDrawer(true)}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Drawer anchor="left" open={drawerOpen} onClose={toggleDrawer(false)}>
+        {drawer}
+      </Drawer>
+    </>
+  );
+}
+
+export default Header;
